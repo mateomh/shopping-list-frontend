5e1bacfedea510a839bd7a756b23d1fcaa265bd0
Mateo mojica <mmojicah.84@gmail.com>
2022-11-08 21:19:37 -0500

    Create the skeleton for the navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,38 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { 
+  faHouse,
+  faBasketShopping,
+  faCartShopping,
+} from '@fortawesome/free-solid-svg-icons';
+// import { solid, regular, brands, icon } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- import styles to be used
+import './NavBar/styles.css';
+
+const NavBar:React.FC = () => {
+  return(
+    <div className="navbar">
+      <a 
+        href="/"
+        className='link'
+      >
+        <FontAwesomeIcon icon={faHouse} />
+        HOME  
+      </a>
+      <a 
+        href="/products"
+        className='link'
+      >
+        <FontAwesomeIcon icon={faBasketShopping} />
+        PRODUCTS 
+      </a>
+      <a 
+        href="/products"
+        className='link'
+      >
+        <FontAwesomeIcon icon={faCartShopping} />
+        Shopping List 
+      </a>
+    </div>
+  );
+}
+
+export default NavBar;
