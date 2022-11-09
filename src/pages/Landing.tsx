ad10a5f349307445c9483c631e784a386f9cc32f
Mateo mojica <mmojicah.84@gmail.com>
2022-11-08 21:20:34 -0500

    Create mock for the lading page

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,21 @@
+const Landing:React.FC = () => {
+
+  return(
+    <div>
+      <hr />
+      <h1>Navbar</h1>
+      <hr />
+      <br />
+      <hr />
+      Content
+      <hr />
+      <br />
+      <hr />
+      Footer
+      <hr />
+    </div>
+  );
+
+}
+
+export default Landing;
