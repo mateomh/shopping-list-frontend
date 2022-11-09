7a447bae7a2dceb2d70c983ded66c00723298dcf
Mateo mojica <mmojicah.84@gmail.com>
2022-11-08 21:21:05 -0500

    Create mock for product display page

---
+++ src/pages/ProductsDisplay.tsx
@@ -0,0 +1,23 @@
+const ProductsDisplay:React.FC = () => {
+  return(
+    <div>
+      <hr />
+      <h1>Navbar</h1>
+      <hr />
+      <br />
+      <hr />
+      Banner
+      <hr />
+      <br />
+      <hr />
+      Table with products
+      <hr />
+      <br />
+      <hr />
+      Footer
+      <hr />
+    </div>
+  );
+} 
+
+export default ProductsDisplay;
