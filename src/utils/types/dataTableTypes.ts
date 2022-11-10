ef3c44b117f7d22f1b58fedb29efb69c6455dd96
Mateo mojica <mmojicah.84@gmail.com>
2022-11-09 22:00:51 -0500

    Create data table types

---
+++ src/utils/types/dataTableTypes.ts
@@ -0,0 +1,10 @@
+export interface ProductData {
+  id: number;
+  name: string;
+  quantity: string;
+  description: string;
+  image_url: string;
+  category_id: number;
+  created_at: string;
+  updated_at: string;
+}
